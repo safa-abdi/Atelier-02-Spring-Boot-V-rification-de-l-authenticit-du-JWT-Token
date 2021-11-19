40a4fc9f2fe2bdfe6cd7f29e504efca099ddae3a
safa-abdi <80724587+safa-abdi@users.noreply.github.com>
2021-11-19 20:21:10 +0100

    Delete users-microservice directory

--- users-microservice/src/main/java/com/safa/users/repos/RoleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.safa.users.repos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.safa.users.entities.Role;
-public interface RoleRepository extends JpaRepository<Role, Long> {
-Role findByRole(String role);
-}
--- users-microservice/src/main/java/com/safa/users/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.safa.users.repos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.safa.users.entities.User;
-public interface UserRepository extends JpaRepository<User, Long> {
-
-User findByUsername(String username);
-
-}
